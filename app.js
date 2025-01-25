45ad75afba0345c06a763024753b054a11a050c2
LoudAsh21 <thiagosancho2006@gmail.com>
2025-01-25 00:20:35 -0300

    Listagem de amigos

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 let listaAmigos = [];
+let inputTextAmigo = document.getElementById("amigo");
+let listTextAmigos = document.getElementById("listaAmigos");
 
+function adicionarAmigo() {
+  if (!inputTextAmigo.value) {
+    alert("Nome não pode ser vazio!");
+    return;
+  }
+  listaAmigos.push(inputTextAmigo.value);
+  atualizarLista(inputTextAmigo.value);
+}
+
+function atualizarLista(nome) {
+  let novaLinha = document.createElement("li");
+  novaLinha.textContent = nome;
+
+  listTextAmigos.appendChild(novaLinha);
+}
